1da442cc4e41ca4a7ab191812c74d472d135187a
danlerg <homegreatewint@gmail.com>
2024-02-07 12:00:54 +0700

    clone script

---
+++ clone_script.sh
@@ -0,0 +1,9 @@
+mkdir suckless
+cd suckless
+git clone https://github.com/danlerg/dwm
+git clone https://github.com/danlerg/dmenu
+git clone https://github.com/danlerg/slstatus
+cd ..
+git clone https://github.com/danlerg/dotfiles
+git clone https://github.com/karlstav/cava
+git clone https://github.com/xorg62/tty-clock
